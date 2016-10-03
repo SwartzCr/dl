951dfeed5a3f0ee8e53e71095629e21962b4996a
Noah Swartz <swartzcr@gmail.com>
2016-10-03 14:24:53 -0400

    add fake page

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 app = Flask(__name__)
 
 @app.route('/')
 def base():
-    response = app.make_response(render_template('base.html'))
+    passcode = request.cookies.get('passcode')
+    if passcode == "happy birthday":
+        response = app.make_response(render_template('base.html'))
+    else:
+        response = app.make_response(send_from_directory('static', 'seller.html'))
     return response
 
 @app.route('/robots.txt')
@@ -13,5 +17,5 @@ def serve_static():
 @app.route('/spacer.gif')
 def secret():
     response = app.make_response(send_from_directory(app.static_folder, 'spacer.gif'))
-    response.set_cookie('passcode', value='bananas foster')
+    response.set_cookie('passcode', value='happy birthday')
     return response
